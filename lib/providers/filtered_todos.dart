576f320c46ffe907b48645a302f037873cd66c3d
hyuem <uhh0701@gmail.com>
2022-05-27 23:44:17 +0900

    ch 44. FilteredTodosSate and FilteredTodosChangeNotifier

---
+++ lib/providers/filtered_todos.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import 'package:todo_provider/models/todos_model.dart';
+import 'package:todo_provider/providers/todo_filter.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+import 'package:todo_provider/providers/todo_search.dart';
+
+class FilteredTodosState extends Equatable {
+  final List<Todo> filteredTodos;
+  FilteredTodosState({
+    required this.filteredTodos,
+  });
+
+  factory FilteredTodosState.initial() {
+    return FilteredTodosState(filteredTodos: []);
+  }
+
+  FilteredTodosState copyWith({
+    List<Todo>? filteredTodos,
+  }) {
+    return FilteredTodosState(
+      filteredTodos: filteredTodos ?? this.filteredTodos,
+    );
+  }
+
+  @override
+  List<Object> get props => [filteredTodos];
+}
+
+class FilteredTodos with ChangeNotifier {
+  FilteredTodosState _state = FilteredTodosState.initial();
+  FilteredTodosState get state => _state;
+
+  ///
+  /// 여러번 호출
+  /// 1. 의존하는 값을 처음으로 얻을 때.
+  /// 2. 의존하는 값이 변할 때마다
+  void update(
+    TodoFilter todoFilter,
+    TodoSearch todoSearch,
+    TodoList todoList,
+  ) {
+    List<Todo> _filteredTodos;
+
+    switch (todoFilter.state.filter) {
+      case Filter.active:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => !todo.completed).toList();
+        break;
+      case Filter.completed:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => todo.completed).toList();
+        break;
+      case Filter.all:
+      default:
+        _filteredTodos = todoList.state.todos;
+        break;
+    }
+
+    if (todoSearch.state.searchTerm.isNotEmpty) {
+      _filteredTodos = _filteredTodos
+          .where(((Todo todo) =>
+              todo.desc.toLowerCase().contains(todoSearch.state.searchTerm)))
+          .toList();
+    }
+
+    _state = _state.copyWith(filteredTodos: _filteredTodos);
+    notifyListeners();
+  }
+}
